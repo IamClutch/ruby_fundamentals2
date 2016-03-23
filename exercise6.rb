05653286b5e7a0b77b5793d169127d886f750342
Amin Shafiee <Amin@Amins-MacBook-Pro.local>
2016-03-23 16:02:20 -0400

    exercise6.rb

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,6 @@
-grocery_list = *["carrots", "toilet paper", "apples"]
-grocery_list << "rice"
-grocery_list.count
-puts grocery_list
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+puts grocery_list.map! {|item| "*" + item}
+grocery_list << "*rice"
 puts grocery_list.length
 if grocery_list.include?("banana")
   puts "You need to pick up bananas"
@@ -9,3 +8,5 @@ else
   puts "You dont need to pick up bananas today"
 end
 puts grocery_list[1]
+grocery_list.delete_at(3)
+puts grocery_list.sort
